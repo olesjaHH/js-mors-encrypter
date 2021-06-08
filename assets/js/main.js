4ba2fdc52aa67e18f8d21b2be121dcd5ad9a628e
OlesjaHH <olesja.friesen@gmail.com>
2021-06-08 14:14:44 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+  ];
+  
+
+let input = document.getElementById('text')
+let text = document.getElementById('encrypt')
+let res = document.getElementById('result')
+let exp = document.getElementById('explanation')
+let header = document.getElementById('header')
+
+console.log("test")
+function decode() {
+    let uppercase = input.value.toUpperCase().split("")
+    
+    uppercase.forEach(element => {
+        morseAlphabet.forEach(morse => {
+            /* console.log(morse) */
+            morse.letter
+            if (morse.letter == element) {
+                console.log(morse.morseCode)
+                res.innerHTML += morse.morseCode + ","
+                header.innerHTML = '<h1>Explanation:</h1>'
+                exp.innerHTML += `${element} = ${morse.morseCode} <br>`
+            }
+        }) 
+    });
+}
+decode()
